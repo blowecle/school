1cfed89f0c6e9e698c0e5e32ef41da520c63d0da
Brandon Lowe <BLoweCLE@gmail.com>
2024-03-05 15:01:12 -0600

    finished lab5, and added debug.txt as a default debug text file for future projects

--- lab1.cpp
+++ lab1.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
+#include<fstream>
 using namespace std;
 
 int main() {
-    cout << "What is the width?" << endl;
-    
+
     float width;
+    float length;
+
+    ofstream debugFile("debug.txt");
+
+    cout << "What is the width?" << endl;
     
     cin >> width;
 
-    cout << "What is the length?" << endl;
+    while(width <= 0){
+        cout << "Please enter a number greater than zero: ";
+        cin >> width;
+    }
 
-    float length;
+    cout << "What is the length?" << endl;
 
     cin >> length;
 
+    while(length <= 0){
+        cout << "Please enter a number greater than zero: ";
+        cin >> length;
+    }
+
     float area = width * length;
 
+    if(debugFile){
+        debugFile << "The length is: " << length << ", the width is: " << width << " and the area is " << length << " * " << width << " = " << area;
+    }
+
     cout << "The area of the rectangle is " << area << endl;
+
+    debugFile.close();
+
+    return 0;
 }
--- lab5.cpp
+++ lab5.cpp
@@ -3,12 +3,13 @@
 #include<fstream>
 using namespace std;
 
+float calculateAverageRainfall(float rainfall, int months);
+
 int main(){
     
     //var declaration
     float rainfall, 
-        totalRainfall = 0, 
-        averageRainfall;
+    totalRainfall = 0;
 
     int months = 0;
 
@@ -20,6 +21,7 @@ int main(){
     //verify file exists and read from file
     if(inputFile){
         
+        //read in the start and ending months from the inputFile
         inputFile >> month;
         startMonth = month;
 
@@ -28,12 +30,25 @@ int main(){
 
         //while there is still rainfall data to read, add the rainfall to totalRainfall and increment months
         while(inputFile >> rainfall){
+            
+            //if it fainfall value is greater than zero, add it to the total
+            if(rainfall >= 0){
+
             totalRainfall += rainfall;
             months++;
+            
+            } else {
+                //if a rainfall value is negative, the data is invalid.  Display error and close the file
+                cout << "Error: Negative rainfall value: , " << rainfall << " encountered in file.";
+                inputFile.close();
+            }
         }
     } else {
+
+        //if file does not exist display error
         cout << "File not found." << endl;
         return 1;
+
     }
 
     //close file
@@ -47,12 +62,18 @@ int main(){
         //if months is not 0 (data was read), calculate averageRainfall
     } else {
 
-        averageRainfall = totalRainfall / months;
-
         //output
         cout << "The total rainfall for " << startMonth << " - " << endMonth << " is " << totalRainfall << " inches." << endl;
-        cout << "And the average monthly rainfall is " << averageRainfall << " inches." << endl;
+        cout << "And the average monthly rainfall is " << calculateAverageRainfall(totalRainfall, months) << " inches." << endl;
 
         return 0;
     }
+}
+
+//calculateAverageRainfall takes in the total rainfall as a float and the number of months as an int
+//it will return a float for the value of the average rainfall per month
+float calculateAverageRainfall(float rainfall, int months){
+    
+    float averageRainfall = rainfall / months;
+    return averageRainfall;
 }
