bf72eccabec9aa0ee990ac5de38e50f0363eb6ea
Brandon Lowe <BLoweCLE@gmail.com>
2024-03-23 10:44:11 -0500

    debugging

--- lab6.cpp
+++ lab6.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 #include<string>
 #include<iomanip>
+#include<cstdlib>
 using namespace std;
 
 //function prototypes
 void getInput(int &spoolsOrdered, int &spoolsInStock, float &specialShippingCharge);
 void displayOrderInfo(int spoolsOrdered, int spoolsInStock, float specialShippingCharge);
 bool isValidOrder(int input);
+bool isInStock(int spoolsOrdered, int spoolsInStock);
 
 //constant definitions
 const int SHIPPING_CHARGE = 10;
 const int SPOOL_COST = 100;
 
 int main(){
+    //clear previous output
+    system("clear");
 
     //var declaration
     int spoolsOrdered,
@@ -39,7 +43,7 @@ void getInput(int &spoolsOrdered, int &spoolsInStock, float &specialShippingChar
 
     cout << "How many spools are in stock?" << endl;
     cin >> spoolsInStock;
-    cout << "spoolsInStock: " << spoolsInStock << "\n";
+
     while(spoolsInStock < 0){
         cout << "Please enter a number greater than or equal to zero." << endl;
         cin >> spoolsInStock;
@@ -55,30 +59,46 @@ void getInput(int &spoolsOrdered, int &spoolsInStock, float &specialShippingChar
 }
 
 void displayOrderInfo(int spoolsOrdered,int spoolsInStock ,float specialShippingCharge){
-    cout << "Spools ready to ship: " << spoolsOrdered << endl;
+    cout << "Spools ordered: " << spoolsOrdered << endl;
     cout << "Spools in stock: " << spoolsInStock << endl;
     
-    if(spoolsOrdered > spoolsInStock){
+    if(!isInStock(spoolsOrdered, spoolsInStock)){
         cout << "Spools on backorder: " << spoolsOrdered - spoolsInStock << endl;
     } else {
         cout << "Spools on backorder: 0" << endl;
     }
 
-    if(spoolsInStock > spoolsOrdered){
+    if(isInStock(spoolsOrdered, spoolsInStock)){
         cout << "Subtotal: $" << spoolsOrdered * SPOOL_COST << endl;
     } else {
         cout << "Subtotal: $" << spoolsInStock * SPOOL_COST << endl;
     }
     
     if(specialShippingCharge > 0){
-        cout << "Shipping: $" << (SHIPPING_CHARGE * spoolsOrdered) + specialShippingCharge << endl;
+        if(isInStock(spoolsOrdered, spoolsInStock)){
+            cout << "Shipping: $" << (SHIPPING_CHARGE * spoolsOrdered) + specialShippingCharge << endl;
+            } else {
+            cout << "Shipping: $" << (SHIPPING_CHARGE * spoolsInStock) + specialShippingCharge << endl;
+            }
     } else {
-        cout << "Shipping: $" << (SHIPPING_CHARGE * spoolsOrdered) << endl;
+        if(isInStock(spoolsOrdered, spoolsInStock)){
+            cout << "Shipping: $" << (SHIPPING_CHARGE * spoolsOrdered) << endl;
+        } else {
+            cout << "Shipping: $" << (SHIPPING_CHARGE * spoolsInStock) << endl;
+        }
     }
 
-    cout << "Total: $" << (spoolsOrdered * SPOOL_COST) + (SHIPPING_CHARGE * spoolsOrdered) + specialShippingCharge << endl;
+    if(isInStock(spoolsOrdered, spoolsInStock)){
+        cout << "Total: $" << (spoolsOrdered * SPOOL_COST) + (SHIPPING_CHARGE * spoolsOrdered) + specialShippingCharge << endl;
+    } else {
+        cout << "Total: $" << (spoolsInStock * SPOOL_COST) + (SHIPPING_CHARGE * spoolsInStock) + specialShippingCharge << endl;
+    }
 }
 
 bool isValidOrder(int input){
     return input > 0;
+}
+
+bool isInStock(int spoolsOrdered, int spoolsInStock){
+    return spoolsOrdered <= spoolsInStock;
 }
